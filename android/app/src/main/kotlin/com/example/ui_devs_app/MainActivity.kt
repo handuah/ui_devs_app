54dad3b73d333b68bce7b47a66505480a3fd43ca
handuah <handuah@gmail.com>
2024-07-30 19:01:43 +0000

    init commit

---
+++ android/app/src/main/kotlin/com/example/ui_devs_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ui_devs_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/screen_one.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// https://www.behance.net/gallery/200589547/Fintech-Wallet-App-UI-UX-Design?tracking_source=search_projects%7Cmobile+ui+app
+
+class ScreenOne extends StatefulWidget {
+  const ScreenOne({super.key});
+
+  @override
+  State<ScreenOne> createState() => _ScreenOneState();
+}
+
+class _ScreenOneState extends State<ScreenOne> {
+  @override
+  Widget build(BuildContext context) {
+    double h = MediaQuery.of(context).size.height;
+    double w = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Color(0xFF0D0D0D),
+      body: SafeArea(
+        child: ListView(
+          padding:
+              EdgeInsets.symmetric(vertical: h * 0.02, horizontal: w * 0.04),
+          children: [
+            Container(
+              height: h * 0.30,
+              width: w,
+              decoration: BoxDecoration(
+                color: Color(0xFFC6FE1E),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: h * 0.02,
+                  horizontal: w * 0.04,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const CircleAvatar(
+                              radius: 35.0,
+                              backgroundColor: Colors.blue,
+                              foregroundImage: NetworkImage(
+                                  'https://megaport.hu/media/king-include/uploads/2023/10/906363-female-avatar-profile-picture-013.jpg'),
+                            ),
+                            SizedBox(
+                              width: w * 0.02,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Jordan Smith',
+                                  style: GoogleFonts.anekTelugu(
+                                    fontSize: h * 0.024,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                // SizedBox(
+                                //   height: h * 0.02,
+                                // ),
+                                Text(
+                                  'Accra, Ghana',
+                                  style: GoogleFonts.anekTelugu(
+                                    // fontSize: h * 0.024,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        CircleAvatar(
+                          radius: 24.0,
+                          backgroundColor: Colors.black.withOpacity(0.1),
+                          child: const Icon(
+                            Icons.notifications_active,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: h * 0.03,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Total Balance',
+                          style: GoogleFonts.anekTelugu(
+                            fontSize: h * 0.020,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              '\$',
+                              style: GoogleFonts.anekTelugu(
+                                // fontSize: h * 0.024,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                            Text(
+                              '5060.00',
+                              style: GoogleFonts.anekTelugu(
+                                fontSize: h * 0.050,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: h * 0.004,
+                    ),
+                    Text(
+                      '*** *** *** 7653',
+                      style: GoogleFonts.anekTelugu(
+                        fontSize: h * 0.024,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
